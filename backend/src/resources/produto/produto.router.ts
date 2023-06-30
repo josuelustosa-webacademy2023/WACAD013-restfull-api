bfcd50251cc04f41b3038bd7a522ae17e2c43a6b
Josué Lustosa <josuelustosa.job@gmail.com>
2023-06-29 21:25:22 -0400

    corrigindo routes

--- backend/src/resources/produto/produto.router.ts
+++ backend/src/resources/produto/produto.router.ts
@@ -4,9 +4,9 @@ import produtoController from './produto.controller';
 const router = Router();
 
 router.get('/', produtoController.index);
-router.post('/', produtoController.create);
-router.get('/:id', produtoController.read);
-router.put('/:id', produtoController.update);
-router.delete('/:id', produtoController.remove);
+router.post('/create', produtoController.create);
+router.get('/read/:id', produtoController.read);
+router.put('/update/:id', produtoController.update);
+router.delete('delete/:id', produtoController.remove);
 
 export default router;
